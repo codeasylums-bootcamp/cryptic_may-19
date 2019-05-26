66b44d60f22e89a2e66d4f71d3f058cfc67d37c1
malcolm123ssj <minmini1997gopal@gmail.com>
2019-05-26 14:48:17 +0530

    Styling Done

--- fetch.js
+++ fetch.js
@@ -12,13 +12,14 @@ function fetch() {
         let table = document.getElementById("currency");
         console.log(chunks[i]);
         let row = table.insertRow(-1);
-        let cell1 = row.insertCell(0), cell2 = row.insertCell(1), cell3 = row.insertCell(2), cell4 = row.insertCell(3), cell5 = row.insertCell(4), cell6 = row.insertCell(5);
+        let cell1 = row.insertCell(0), cell2 = row.insertCell(1), cell3 = row.insertCell(2), cell4 = row.insertCell(3), cell5 = row.insertCell(4), cell6 = row.insertCell(5), cell7 = row.insertCell(6);
         cell1.innerHTML = chunks[i].name;
         cell2.innerHTML = chunks[i].symbol;
-        cell3.innerHTML = chunks[i].priceUsd;
-        cell4.innerHTML = chunks[i].supply;
-        cell5.innerHTML = chunks[i].marketCapUsd;
-        cell6.innerHTML = chunks[i].volumeUsd24Hr;
+        cell3.innerHTML = '$'+parseFloat(chunks[i].priceUsd).toFixed(4);
+        cell4.innerHTML = (parseFloat(chunks[i].supply)/1.0e+9).toFixed(4)+'bn';
+        cell5.innerHTML = '$'+(parseFloat(chunks[i].marketCapUsd)/1.0e+9).toFixed(4)+'bn';
+        cell6.innerHTML = '$'+(parseFloat(chunks[i].volumeUsd24Hr)/1.0e+9).toFixed(4)+'bn';
+        cell7.innerHTML = (parseFloat(chunks[i].changePercent24Hr).toFixed(4))+'%';
         }
     });
 

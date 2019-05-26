b4e58dcd1119da599202a68822806b302813c54c
malcolm123ssj <minmini1997gopal@gmail.com>
2019-05-26 12:24:28 +0530

    Resolved table issues and errors

--- fetch.js
+++ fetch.js
@@ -1,43 +1,28 @@
 function fetch() {
 
-    var https = require('https');
-
-    var options = {
-        'method': 'GET',
-        'hostname': 'api.coincap.io',
-        'path': '/v2/assets',
-        'headers': {
-        }
-    };
-
-    var req = https.request(options, function (res) {
     var chunks = [];
 
-    res.on("data", function (chunk) {
-        chunks.push(chunk);
+    let xhr = new XMLHttpRequest;
+    xhr.addEventListener('load',function(){
+        let obj = JSON.parse(this.responseText);
+        for (let i = 0; i < obj.data.length; i++) {
+            let chunk = obj.data[i];
+            chunks.push(chunk);
+            
+        let table = document.getElementById("currency");
+        console.log(chunks[i]);
+        let row = table.insertRow(-1);
+        let cell1 = row.insertCell(0), cell2 = row.insertCell(1), cell3 = row.insertCell(2), cell4 = row.insertCell(3), cell5 = row.insertCell(4), cell6 = row.insertCell(5);
+        cell1.innerHTML = chunks[i].name;
+        cell2.innerHTML = chunks[i].symbol;
+        cell3.innerHTML = chunks[i].priceUsd;
+        cell4.innerHTML = chunks[i].supply;
+        cell5.innerHTML = chunks[i].marketCapUsd;
+        cell6.innerHTML = chunks[i].volumeUsd24Hr;
+        }
     });
 
-    res.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        console.log(body.toString());
-    });
+    xhr.open('GET', "https://api.coincap.io/v2/assets");
+    xhr.send();
 
-    res.on("error", function (error) {
-        console.error(error);
-    });
-    });
-
-    req.end();
-
-    var table = document.getElementById("currency");
-    for (var i=0;i<chunks.length;i++) {
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0), cell2 = row.insertCell(1), cell3 = row.insertCell(2), cell4 = row.insertCell(3), cell5 = row.insertCell(4), cell6 = row.insertCell(5);
-        cell1.innerHTML = chunks.name;
-        cell2.innerHTML = chunks.symbol;
-        cell3.innerHTML = chunks.priceUsd;
-        cell4.innerHTML = chunks.supply;
-        cell5.innerHTML = chunks.marketCapUsd;
-        cell6.innerHTML = chunks.volumeUsd24Hr;
-    }
 }

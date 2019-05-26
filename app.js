bf599091b3b4e1e778e457da080c1aa41e4a2a19
malcolm123ssj <minmini1997gopal@gmail.com>
2019-05-26 18:41:01 +0530

    Login and SignUp essentials

--- app.js
+++ app.js
@@ -3,6 +3,20 @@ var fs = require('fs');
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017/(something)';
+MongoClient.connect(url, function(err, db){
+    if(err) {
+        return console.dir(err);
+    }
+    console.log("Connected Successfully");
+});
+
+const InsertDocument = function(db, callback) {
+    const collection = db.collection('úsers');
+    collection.insert()
+}
 
 var port = 3000;
 var app = express();
